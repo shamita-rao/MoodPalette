c65f81c5a7d79afdfa7cfa56cc958bf32df42e4b
Shamita Rao <shamita@umich.edu>
2025-11-24 00:15:01 -0500

    Remove Secrets

--- Secrets.js
+++
@@ -1,9 +0,0 @@
-// Firebase config
-export const firebaseConfig = {
-  apiKey: "AIzaSyDLyaPSdkYBGUTj_wt9efeosVi5FgFF7Hs",
-  authDomain: "moodpalette-538b3.firebaseapp.com",
-  projectId: "moodpalette-538b3",
-  storageBucket: "moodpalette-538b3.firebasestorage.app",
-  messagingSenderId: "1096597860736",
-  appId: "1:1096597860736:web:5545bb2df6e11b117ad165"
-};
